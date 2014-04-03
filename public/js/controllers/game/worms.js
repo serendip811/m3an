8d3f3b84d4b94db27ccc37c698379d14894cbee6
serendip811 <serendip811@gmail.com>
2014-04-03 17:24:53 +0900

    worm move2

--- public/js/controllers/game/worms.js
+++ public/js/controllers/game/worms.js
@@ -16,12 +16,15 @@ angular.module('mean.game.worms').controller('GameWormsController', ['$scope','$
         worms.push(new Worm(50,50));
 
         var timer = setInterval(function(){
-            worms[0].moveRight();
+            var a = Math.floor((Math.random()*2)+1);
+            console.log(a);
+            if(a > 1){
+                worms[0].moveRight();
+            }else{
+                worms[0].moveDown();
+            }
+            
         }, 1000);
-
-        var timer2 = setInterval(function(){
-            worms[0].moveDown();
-        }, 700);
 
     };
 
@@ -40,22 +43,24 @@ angular.module('mean.game.worms').controller('GameWormsController', ['$scope','$
     var Worm = function(x, y){
         this.coords = [];
         this.coords.push(new Coord(x, y));
-        this.coords.push(new Coord(x+10, y+10));
-        this.coords.push(new Coord(x+20, y+20));
-        this.coords.push(new Coord(x+30, y+30));
+        this.coords.push(new Coord(x-10, y));
+        this.coords.push(new Coord(x-20, y));
+        this.coords.push(new Coord(x-30, y));
+        this.coords.push(new Coord(x-40, y));
+        this.coords.push(new Coord(x-50, y));
         this.last_c = null;
 
         this.moveLeft = function(){this.move(-10,0);};
         this.moveRight = function(){this.move(10,0);};
         this.moveUp = function(){this.move(0,-10);};
         this.moveDown = function(){this.move(0,10);};
-        this.move = function(x, y){
+        this.move = function(m_x, m_y){
             for(var i=this.coords.length-1;i>0;i--){
                 this.coords[i].x = this.coords[i-1].x;
                 this.coords[i].y = this.coords[i-1].y;
             }
-            this.coords[0].x += x;
-            this.coords[0].y += y;
+            this.coords[0].x += m_x;
+            this.coords[0].y += m_y;
             render();
         };
     };
